01b88da605cf9a9411d980ec79cdacf31dbaae4e
kashishdodeja <kashishdodeja447@gmail.com>
2024-10-14 14:19:17 +0530

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LinkedList from './LinkedList';
 import Stacks from './Stacks';
 import Queues from './Queues';
 import Auth from './Login/Auth';
 import UserProfile from './Profile/profile';
+import HomePage from './HomePage'; // Ensure this path is correct
+
 const App = () => {
   return (
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Auth />} />
-          <Route path="/linkedlist" element={<LinkedList />} />
-          <Route path="/stack" element={<Stacks />} />
-          <Route path="/queue" element={<Queues />} />
-          <Route path="/profile" element={<UserProfile />} />
-        </Routes>
-      </Router>
+    <BrowserRouter>
+      <Routes>
+        {/* Route for the homepage */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Route for the login page */}
+        <Route path="/login" element={<Auth />} />
+
+        {/* Route for Linked List page */}
+        <Route path="/linkedlist" element={<LinkedList />} />
+
+        {/* Route for Stack page */}
+        <Route path="/stack" element={<Stacks />} />
+
+        {/* Route for Queue page */}
+        <Route path="/queue" element={<Queues />} />
+
+        {/* Route for the user profile page */}
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
